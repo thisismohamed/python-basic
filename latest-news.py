16cb4ff610037d605ec7d298f1b55d29c14c3d4a
Mohamed <itslambda3@gmail.com>
2024-12-06 13:04:54 +0200

    Create latest-news.py

---
+++ latest-news.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautfulSoup
+
+url = "https://news.google.com/news/rss"
+
+response = requests.get(url)
+html_content = response.text
+
+soup = BeautifulSoup(html_content, 'xml')
+items = soup.find_all('item')
+
+for item in items:
+  print(item.title.text)
+  print(item.link.text)
+  print(item.pubDate.text)
+  print("-" * 55)
